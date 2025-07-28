76d0219f3e8b94b0ce051c0d6c7ff706703de112
dplaza1 <dplaza1@stevens.edu>
2025-07-28 13:50:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,17 +24,17 @@ beta = st.number_input("Beta [deg]", min_value=5.0, max_value=45.0, value=20.0)
 cv = st.number_input("Cv [-]", min_value=0.8, value=1.3)
 lcg = st.number_input("LCG [%L]", min_value=20.0, max_value=50.0, value=38.0)
 tao = st.number_input("Tao [deg]", min_value=1.0, value=3.6)
-fn = st.number_input("Froude Number [-]", min_value=0.4, value=1.0)
 h13b = st.number_input("H1/3/b", min_value=0.10, value=0.43)
+fn = st.number_input("Froude Number [-]", min_value=0.4, value=1.0)  # This is only used for nCG
 
 if st.button("Predict"):
-    # Predict nCG
+    # Predict nCG (requires fn)
     X1 = np.array([[beta, cv, lcg, tao, fn, h13b]])
     X1_scaled = scaler_X1.transform(X1)
     pred_ncg = gpr_ncg_model.predict(X1_scaled)[0]
 
-    # Predict nBow (using pred_ncg)
-    X2 = np.array([[beta, cv, lcg, tao, fn, h13b, pred_ncg]])
+    # Predict nBow (uses pred_ncg, but NOT fn)
+    X2 = np.array([[beta, cv, lcg, tao, h13b, pred_ncg]])
     X2_scaled = scaler_X2.transform(X2)
     pred_nbow = etr_nbow_model.predict(X2_scaled)[0]
 
@@ -48,6 +48,6 @@ if st.button("Predict"):
         "Cv": cv,
         "LCG": lcg,
         "Tao": tao,
-        "Fn": fn,
-        "H1/3/b": h13b
+        "H1/3/b": h13b,
+        "Froude Number": fn
     })
